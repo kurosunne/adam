a81f80abdd96a028f275f7e05cdfb74d9385c77c
Anderson Soegiharto <83352641+Soeg-Cronus@users.noreply.github.com>
2023-01-04 20:52:24 +0700

    Activity polisi ke fragment
    cuma bisa pindah, belum passing data

---
+++ app/src/main/java/com/adam/LaporanFragment.kt
@@ -0,0 +1,19 @@
+package com.adam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class LaporanFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_laporan, container, false)
+    }
+}
