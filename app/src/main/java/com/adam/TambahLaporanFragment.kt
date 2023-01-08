638395931a5d30cc51044f911f26cbd2cce493ec
mikhaelc09 <mikhael.chris09@gmail.com>
2023-01-08 20:14:58 +0700

    Create fragment tambah laporan

--- app/src/main/java/com/adam/TambahLaporanFragment.kt
+++ app/src/main/java/com/adam/TambahLaporanFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 
 class TambahLaporanFragment : Fragment() {
+    private lateinit var lapor_etJudul:EditText
+    private lateinit var lapor_spCategory:Spinner
+    private lateinit var lapor_etLokasi:EditText
+    private lateinit var lapor_mtDeskripsi:EditText
+    private lateinit var lapor_cbKonfirmasi:CheckBox
+    private lateinit var lapor_btLapor:Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +23,21 @@ class TambahLaporanFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_tambah_laporan, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lapor_etJudul = view.findViewById<EditText>(R.id.lapor_etJudul)
+        lapor_spCategory = view.findViewById<Spinner>(R.id.lapor_spCategory)
+        lapor_etLokasi = view.findViewById<EditText>(R.id.lapor_etLokasi)
+        lapor_mtDeskripsi = view.findViewById<EditText>(R.id.lapor_mtDeskripsi)
+        lapor_cbKonfirmasi = view.findViewById<CheckBox>(R.id.lapor_cbKonfirmasi)
+        lapor_btLapor = view.findViewById<Button>(R.id.lapor_btLapor)
+
+        lapor_cbKonfirmasi.setOnCheckedChangeListener { compoundButton, b ->
+
+        }
+
+        lapor_btLapor.setOnClickListener {
+
+        }
+    }
 }
